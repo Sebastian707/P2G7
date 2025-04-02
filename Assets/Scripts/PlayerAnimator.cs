4b2c0063d4629c105662c2078549a9357c549e59
ScandieFox <92449921+ScandieFox@users.noreply.github.com>
2025-04-02 12:01:57 +0200

    Player animation added

---
+++ Assets/Scripts/PlayerAnimator.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    float horizontalMove = 0f;
+    public float runSpeed = 40f;
+
+    private Animator animator;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+    }
+}
